fa3ad09f0336ca4d2cec104195fb85544bd91ff4
Jeremy <jeremy.gandase@gmail.com>
2022-10-31 14:24:50 +0700

    Added TP Module 6

--- JeremyGandaPandapotanJSleepKM/JSleep.java
+++ JeremyGandaPandapotanJSleepKM/JSleep.java
@@ -1,6 +1,10 @@
 package JeremyGandaPandapotanJSleepKM;
 import java.util.*;
-import java.sql.Date;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 /**
  * Write a description of class JSleep here.
@@ -11,6 +15,29 @@ import java.sql.Date;
 
 public class JSleep
 {
+	class Country{
+		public String name;
+		public int population;
+		public List<String> listOfStates;
+	}
+	
+	public static void main (String[] args) {
+		String filepath= "E:\\Kuligan\\Sem 3\\OOP\\Modul 1\\JSleep\\city.json";
+		Gson gson = new Gson();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			Country input = gson.fromJson(br, Country.class);
+			System.out.println("name: " + input.name);
+			System.out.println("population: " + input.population);
+			System.out.println("states : ");
+			input.listOfStates.forEach(state -> System.out.println(state));
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
     public static void main(String args[]){
         
     	ArrayList<Room> RoomSerialized = new ArrayList<Room>();
@@ -27,6 +54,7 @@ public class JSleep
 
         return room;
     }
+    */
 }
 
 
