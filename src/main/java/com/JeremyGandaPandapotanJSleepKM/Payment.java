3534d6bf3bcf90bc2aa769715e49f2f0f00cad01
Jeremy <jeremy.gandase@gmail.com>
2022-12-11 22:40:44 +0700

    Fix Payment & Room

--- src/main/java/com/JeremyGandaPandapotanJSleepKM/Payment.java
+++ src/main/java/com/JeremyGandaPandapotanJSleepKM/Payment.java
@@ -8,74 +8,122 @@ import java.text.*;
  * @version (a version number or a date)
  */
 public class Payment extends Invoice {
+    /**
+     * The date on which the rental period ends.
+     */
     public Date to;
+
+    /**
+     * The date on which the rental period begins.
+     */
     public Date from;
-    private int roomId;
 
-    public Payment(int id, int buyerId, int renterId, int roomId, Date fromDate, Date toDate) {
+    /**
+     * The total price of the rental.
+     */
+    public double totalPrice;
+
+    /**
+     * The ID of the room that is being rented.
+     */
+    public int roomId;
+
+    /**
+     * Creates a new `Payment` with the given buyer, renter, and rental information.
+     *
+     * @param buyerId the ID of the buyer making the payment.
+     * @param renterId the ID of the renter who is receiving the payment.
+     * @param roomId the ID of the room being rented.
+     * @param from the date on which the rental period begins.
+     * @param to the date on which the rental period ends.
+     */
+    public Payment(int buyerId, int renterId, int roomId, Date from, Date to) {
         super(buyerId, renterId);
+        this.to = to;
+        this.from = from;
         this.roomId = roomId;
-        this.from = fromDate;
-        this.to = toDate;
     }
 
-    public Payment(Account buyer, Renter renter, int roomId, Date fromDate, Date toDate) {
+    /**
+     * Creates a new `Payment` with the given buyer, renter, and rental information.
+     *
+     * @param buyer the buyer making the payment.
+     * @param renter the renter who is receiving the payment.
+     * @param roomId the ID of the room being rented.
+     * @param from the date on which the rental period begins.
+     * @param to the date on which the rental period ends.
+     */
+    public Payment(Account buyer, Renter renter, int roomId, Date from, Date to) {
         super(buyer, renter);
+        this.to = to;
+        this.from = from;
         this.roomId = roomId;
-        this.from = fromDate;
-        this.to = toDate;
-    }
-    
-    
-    public static boolean makeBooking(Date from, Date to, Room room){
-    	Calendar temp = Calendar.getInstance();
-    	if (availability(from, to, room) == true) {
-        while (from.before(to)){
-            room.booked.add(from);
-            temp.setTime(from);
-            temp.add(Calendar.DATE, 1);
-            from = temp.getTime();
-        	}
-        return true;
-    	}
-    	
-    	else {
-    		return false;
-    	}
-    }
-    /*
-    public String getDuration(){
-        SimpleDateFormat SDFormat = new SimpleDateFormat("dd MMMM yyyy");
-        return "" + SDFormat.format(this.from.getTime()) + " - " + SDFormat.format(this.to.getTime());
-    }
-*/
-    public String print() {
-        return "\nRoomId = " + roomId + "\nFrom = " + from + "\nTo = " + to;
     }
 
-    public int getRoomId() {
-        return roomId;
+    /**
+     * Returns whether the given rental period is available for the specified room.
+     *
+     * @param from the date on which the rental period begins.
+     * @param to the date on which the rental period ends.
+     * @param room the room being rented.
+     * @return `true` if the rental period is available for the room, `false` otherwise.
+     */
+    public static boolean availability(Date from, Date to, Room room) {
+        if (to.compareTo(from) < 0 || to.compareTo(from) == 0) {
+            return false;
+        } else {
+            for (Date inc : room.booked) {
+                if (from.equals(inc)) {
+                    return false;
+                } else if (from.before(inc)) {
+                    if (from.before(inc) && to.after(inc)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 
-    public static boolean availability(Date from, Date to, Room room){
-        
-        //error handling
-        if(to.before(from))
-        {
+    /**
+     * Makes a booking for the specified rental period in the given room.
+     *
+     * @param from the date on which the rental period begins.
+     * @param to the date on which the rental period ends.
+     * @param room the room being rented.
+     * @return `true` if the booking was successful, `false` otherwise.
+     */
+    public static boolean makeBooking(Date from, Date to, Room room) {
+        if (availability(from, to, room) == true) {
+            Calendar tmp = Calendar.getInstance();
+            while (from.before(to)) {
+                room.booked.add(from);
+                tmp.setTime(from);
+                tmp.add(Calendar.DATE, 1);
+                from = tmp.getTime();
+            }
+            return true;
+        } else {
             return false;
         }
-        
-        else {
-        for(Date index : room.booked) {
-        	if(from.equals(index)) {
-        		return false;
-        	}
-        	
-        	else if(from.before(index) && to.after(index)) {
-        		return false;
-        	}
-        }
-        return true;
-        }
+    }
+
+    /**
+     * Returns a string representation of the `Payment`, including the dates of the rental period and the ID of the rented room.
+     *
+     * @return a string representation of the `Payment`.
+     */
+    public String print() {
+        String prin = "Hasil Output\n" + "To : " + this.to + "\nFrom : " + this.from + "\nRoom ID : " + this.roomId;
+        return prin;
+    }
+
+    /**
+     * Returns the ID of the room associated with the `Payment`.
+     *
+     * @return the ID of the room associated with the `Payment`.
+     */
+    public int getRoomId() {
+        return this.roomId;
     }
 }
--- src/main/java/com/JeremyGandaPandapotanJSleepKM/controller/PaymentController.java
+++ src/main/java/com/JeremyGandaPandapotanJSleepKM/controller/PaymentController.java
@@ -4,8 +4,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,52 +31,63 @@ public class PaymentController implements BasicGetController<Payment> {
 
 
     @PostMapping("/{id}/accept")
-    public boolean accept ( @RequestParam int id) {
-    	 Payment payment = Algorithm.<Payment>find(paymentTable, pred -> pred.id == id);
-         if (payment != null && payment.status == Invoice.PaymentStatus.WAITING) {
-             payment.status = Invoice.PaymentStatus.SUCCESS;
-             return true;
-         }
-        return false;
-    }
-
-    @PostMapping("/payment")
-    public boolean cancel (@RequestParam int id) {
-    	Payment payment = Algorithm.<Payment>find(paymentTable, pred -> pred.id == id);
-        if (payment != null && payment.status == Invoice.PaymentStatus.WAITING) {
-            payment.status = Invoice.PaymentStatus.FAILED;
-            Account acc = Algorithm.<Account>find(AccountController.accountTable, pred -> pred.id == payment.buyerId);
-            Room room = Algorithm.<Room>find(RoomController.roomTable, pred -> pred.id == payment.renterId);
-            acc.balance += room.price.price;
+    boolean accept(@PathVariable int id){
+        Payment payCheck = Algorithm.<Payment>find(paymentTable, payment -> payment.id == id);
+        if (Invoice.PaymentStatus.WAITING.toString().equals("WAITING") && payCheck != null ) {
+            payCheck.status = Invoice.PaymentStatus.SUCCESS;
             return true;
+        }else {
+            return false;
         }
-        return false;
+    }
+    
+    /**
+     * The cancel() method cancels the payment with the given ID and updates its status to FAILED.
+     * The method also refunds the buyer's balance with the payment amount.
+     *
+     * @param id The ID of the payment to be cancelled.
+     *
+     * @return true if the payment is successfully cancelled, or false if the payment does not exist or is not in a WAITING status.
+     */
+    @PostMapping("/{id}/cancel")
+    boolean cancel(@PathVariable int id){
+
+        Payment payCheck = Algorithm.<Payment>find(paymentTable, payment -> payment.id == id);
+        Account accountCheck = Algorithm.<Account>find(AccountController.accountTable, account -> account.id == payCheck.buyerId);
+        Room roomCheck = Algorithm.<Room>find(RoomController.roomTable, room -> room.id == payCheck.getRoomId());
+
+        if (payCheck != null && Invoice.PaymentStatus.WAITING.toString().equals("WAITING")) {
+            payCheck.status = Invoice.PaymentStatus.FAILED;
+            accountCheck.balance += roomCheck.price.price;
+            return true;
+        }else{
+            return false;
+        }
     }
 
     @PostMapping("/create")
-    public Payment create (
-    		@RequestParam int buyerId, 
-    		@RequestParam int renterId, 
-    		@RequestParam int roomId, 
-    		@RequestParam String from, 
-    		@RequestParam String to
-    		)throws ParseException {
-    	Account acc = Algorithm.<Account>find(AccountController.accountTable, pred -> pred.id == buyerId && pred.id == buyerId);
-        Room room = Algorithm.<Room>find(RoomController.roomTable, pred -> pred.id == roomId && pred.accountId == roomId);
-    	
-        double price = room.price.price;
-
+    public Payment create( @RequestParam int buyerId, @RequestParam int renterId, @RequestParam int roomId,  @RequestParam String from,@RequestParam String to){
+        Room roomCheck = Algorithm.<Room>find(RoomController.roomTable, room -> room.id == roomId);
+        Account accountCheck = Algorithm.<Account>find(AccountController.accountTable, acc -> acc.id == buyerId);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date fromDate = sdf.parse(from);
-        Date toDate = sdf.parse(to);
-
-        if(acc.balance >= price && acc != null && room != null  ){
-            Payment payment = new Payment(acc.id, buyerId, renterId, roomId, fromDate, toDate);
-            acc.balance -= price;
-            payment.status=Invoice.PaymentStatus.WAITING;
-            payment.makeBooking(fromDate, toDate, room);
-            paymentTable.add(payment);
-            return payment;
+        try {
+            Date fromTgl = sdf.parse(from);
+            Date toTgl = sdf.parse(to);
+            long day = toTgl.getTime() - fromTgl.getTime();
+            double totalPay = roomCheck.price.price * (TimeUnit.MILLISECONDS.toDays(day));
+            if(accountCheck != null && roomCheck != null && totalPay <= accountCheck.balance && Payment.availability(fromTgl, toTgl, roomCheck)){
+                Payment paid = new Payment(buyerId, renterId, roomId, fromTgl, toTgl);
+                accountCheck.balance -= totalPay;
+                paid.totalPrice = totalPay;
+                paid.status = Invoice.PaymentStatus.WAITING;
+                Payment.makeBooking(fromTgl, toTgl, roomCheck);
+                paymentTable.add(paid);
+                return paid;
+            }else{
+                return null;
+            }
+        } catch (ParseException p) {
+            p.printStackTrace();
         }
         return null;
     }
